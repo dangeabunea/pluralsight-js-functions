65cd85171de8e0428eb241181f59dfc415709c3a
Dan Geabunea <dangeabunea@hotmail.com>
2022-10-18 14:24:09 +0300

    Initial commit

---
+++ m01/before/defining-functions.js
@@ -0,0 +1,32 @@
+// Create a new function that converts gallons
+// to liters
+
+/*
+function convertToLiters(gallons){
+    return gallons * 3.785;
+}
+
+console.log(convertToLiters(10));
+ */
+
+/*
+const convertToLiters = function(gallons){
+    return gallons * 3.785;
+}
+
+console.log(convertToLiters(10));
+ */
+
+/*
+const convertToLiters = gallons => gallons * 3.785;
+
+
+console.log(convertToLiters(10));
+ */
+
+/*
+const convertToLiters = new Function("gallons", 'return gallons * 3.785;');
+console.log(convertToLiters(10));
+ */
+
+
