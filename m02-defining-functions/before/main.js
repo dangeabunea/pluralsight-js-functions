6ae66e944656c094aa2ba65c9d0f6dc0784c9cec
Dan Geabunea <dangeabunea@hotmail.com>
2022-10-31 13:35:57 +0200

    Fixed main.js

--- m02-defining-functions/before/main.js
+++ m02-defining-functions/before/main.js
@@ -1,7 +1,11 @@
 // Create a new function that converts gallons
 // to liters: convertToLiters
 
+const convertToLiters = (gallons) => {
+    return gallons * 3.785;
+}
 
+console.log(convertToLiters(10));
 
 
 
