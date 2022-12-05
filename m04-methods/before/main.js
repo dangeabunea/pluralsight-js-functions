fa061240727a01258998a161ab8b9edf60cff235
Dan Geabunea <dangeabunea@hotmail.com>
2022-12-05 12:44:04 +0200

    Fixed methds demo

---
+++ m04-methods/before/main.js
@@ -0,0 +1,7 @@
+class FlightPlan {
+    constructor(callsign = '', departure = '', destination = '') {
+        this.callsign = callsign;
+        this.departure = departure;
+        this.destination = destination;
+    }
+}
